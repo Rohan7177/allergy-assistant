5650dd1a85072ff27544315241e7412bd87cdf46
Rohan Patt <rohpatt2008@gmail.com>
2025-02-07 14:41:23 -0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,18 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
-          {/* This meta tag ensures proper scaling on mobile devices */}
+          {/* Ensures proper scaling on mobile and respects safe areas */}
           <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+          <link rel="icon" href="/favicon.ico" />
         </Head>
-        <body>
+        <body className="bg-gray-100" style={{
+          paddingTop: 'env(safe-area-inset-top)',
+          paddingBottom: 'env(safe-area-inset-bottom)',
+          paddingLeft: 'env(safe-area-inset-left)',
+          paddingRight: 'env(safe-area-inset-right)',
+        }}>
           <Main />
           <NextScript />
         </body>
